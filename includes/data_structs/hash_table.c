054d814f144fdda60c5a2a179f11abb7219f4789
José Rafael <joserafaeltineog@gmail.com>
2026-08-31 15:05:45 -0400

    Base para la implementacion de striped locking en HashTable

--- includes/data_structs/hash_table.c
+++ includes/data_structs/hash_table.c
@@ -1,5 +1,7 @@
 #include "hash_table.h"
 
+// Hash e indices de la tabla
+
 int hash_fn(char *key, int size)
 {
   int current_c, total = 0;
@@ -12,16 +14,57 @@ int hash_fn(char *key, int size)
   return total / size;
 }
 
+int lock_index(int bucket)
+{
+  return (bucket % N_LOCKS);
+}
+
+// Creacion y destruccion de tablas
+
 hash_table *creat_hash_table(void)
 {
   hash_table *table = (hash_table *)malloc(sizeof(hash_table));
-  if (!table)
+  pthread_mutex_t *locks = (pthread_mutex_t *)malloc(N_LOCKS * sizeof(pthread_mutex_t));
+  if (!table || !locks)
     return NULL;
 
   table->capacity = INIT_CAPACITY;
   table->numOfElements = 0;
   table->arr = (Node *)calloc(table->capacity, sizeof(Node));
+  table->locks = locks;
   return table;
+}
+
+void ht_destroy(hash_table *table)
+{
+  if (!table)
+    return;
+
+  for (int ind = 0; ind < table->numOfElements; ind++)
+  {
+    Node *head;
+    if ((head = table->arr[ind]) != NULL)
+    {
+      Node *current_node = head;
+      Node *next_node = current_node->next;
+      while (next_node != NULL)
+      {
+        free(current_node->data->key);
+        free(current_node->data->value);
+        free(current_node->data);
+        free(current_node);
+        current_node = next_node;
+        next_node = next_node->next;
+      }
+    }
+  }
+
+  for (int ind = 0; ind < N_LOCKS; ind++)
+    pthread_mutex_unlock(&table->locks[ind]);
+
+  free(table->locks);
+  free(table);
+  return;
 }
 
 void hash_insert(kvstore *kv, hash_table *table)
@@ -62,9 +105,7 @@ void delete_key(char *key, hash_table *table)
   if ((current_node = get_node_by_key(table->arr[index], key)) != NULL)
   {
     if (current_node->prev == NULL)
-    {
       current_node = current_node->next;
-    }
     else
     {
       current_node->prev->next = current_node->next;
--- includes/data_structs/hash_table.h
+++ includes/data_structs/hash_table.h
@@ -2,20 +2,28 @@
 #define HASH_TABLE_H
 
 #include "linked_list.h"
+#include <pthread.h>
+#include <stdatomic.h>
 
-#define INIT_CAPACITY 10
+#define INIT_CAPACITY 64
+#define N_LOCKS 16
 
 typedef struct hash_table
 {
-  int numOfElements, capacity;
+  atomic_int numOfElements;
+  int capacity;
   Node **arr;
+  pthread_mutex_t *locks;
 } hash_table;
 
 int hash_fn(char *, int);
 hash_table *creat_hash_table(void);
+void ht_destroy(hash_table *);
 void hash_insert(kvstore *, hash_table *);
 kvstore *search(char *, hash_table *);
 void delete_key(char *, hash_table *);
 int modif_key(hash_table *, char *, char *);
+
+int lock_index(int);
 
 #endif
